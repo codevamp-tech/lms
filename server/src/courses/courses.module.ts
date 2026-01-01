aa98a94677741c0c52719acd55a1e6f39427404e
khanshoaibraza004 <khanshoaibraza004@gmail.com>
2026-01-01 19:58:29 +0530

    update price

--- server/src/courses/courses.module.ts
+++ server/src/courses/courses.module.ts
@@ -4,12 +4,16 @@ import { Course, CourseSchema } from './schemas/course.schema';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { Lecture, LectureSchema } from 'src/lectures/schemas/lecture.schema';
+import { Enquiry, EnquirySchema } from 'src/enquiries/schemas/enquiry.schema';
+import { LiveSession, LiveSessionSchema } from 'src/live-session/schemas/live-session.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Course.name, schema: CourseSchema },
       { name: Lecture.name, schema: LectureSchema },
+      { name: Enquiry.name, schema: EnquirySchema },
+      { name: LiveSession.name, schema: LiveSessionSchema },
     ]),
   ],
   providers: [CoursesService],
