af61ccf28918efe263a5bf3c97989c5ac0dcd304
talhaansari692 <talhaansari692@gmail.com>
2026-02-18 11:25:37 +0530

    resolve dependency issue

--- client/components/RichTextEditor.jsx
+++ client/components/RichTextEditor.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
-const RichTextEditor = ({input, setInput}) => {
+const RichTextEditor = ({ input, setInput }) => {
 
-    const handleChange = (content) => {
-        setInput({...input, description:content});
-    }
-   
+  const handleChange = (content) => {
+    setInput({ ...input, description: content });
+  }
+
   return <ReactQuill theme="snow" value={input.description} onChange={handleChange} />;
 }
 export default RichTextEditor
